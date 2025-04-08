ff41d91e1a8b08e91e919828612de3aa8aff96ed
Vanessa Da Silva Lima <200276851+programinggiff@users.noreply.github.com>
2025-04-08 23:08:29 +0000

    Declaração-segunda-carta

--- Desafio-Super-trunfo.c
+++ Desafio-Super-trunfo.c
@@ -5,17 +5,25 @@
 
 int main() {
 
- printf("Desafio  Países\n");
- // Declaração das variáveis para primeira carta.
+     printf("Desafio  Países\n");
+     // Declaração das variáveis para primeira carta.
+     char estado1[4];
+     char cidade1[50];
+     char codigo1[4];
+     int populacao1;
+     float area1;
+     float pib1;
+     int pontosTuristicos1;
 
- char estado1[4];
- char cidade1[50];
- char codigo1[4];
- int populacao1;
- float area1;
- float pib1;
- int pontosTuristicos1;
-
+     // Declaração das variantes para segunda carta.
+     char estado2[4];
+     char cidade2[50];
+     char codigo2[4];
+     int populacao2;
+     float area2;
+     float pib2;
+     int pontosTuristicos2;
+ 
 
 
 
