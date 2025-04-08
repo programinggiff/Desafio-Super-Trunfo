10d1e84eeb4f481e595aa7347f7884b2330ee313
Vanessa Da Silva Lima <200276851+programinggiff@users.noreply.github.com>
2025-04-08 23:03:26 +0000

    Declação-primeira-carta

--- Desafio-Super-trunfo.c
+++ Desafio-Super-trunfo.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+// Desafio Super Trunfo Países.
+// programador Vanessa.
+
 
 int main() {
+
+ printf("Desafio  Países\n");
+ // Declaração das variáveis para primeira carta.
+
+ char estado1[4];
+ char cidade1[50];
+ char codigo1[4];
+ int populacao1;
+ float area1;
+ float pib1;
+ int pontosTuristicos1;
+
 
 
 
